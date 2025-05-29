d40a28b98a0c0906934e30cb05992128b949b255
abralic98 <twogamers1950@gmail.com>
2025-05-29 11:56:21 +0200

    small fixes

--- features/dashboard/servers/components/discovery/DiscoverServers.tsx
+++ features/dashboard/servers/components/discovery/DiscoverServers.tsx
@@ -1,18 +1,17 @@
 import routes from "@/lib/routes";
 import { Globe } from "lucide-react";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React from "react";
 
 export const DiscoverServers = () => {
   const { push } = useRouter();
   return (
-    <div
-      onClick={() => {
-        push(routes.discover);
-      }}
+    <Link
+      href={routes.discover}
       className="bg-sidebar-accent hover:bg-sidebar-hover transition-colors duration-200 cursor-pointer font-bold rounded-full w-14 h-14 flex items-center justify-center"
     >
       <Globe />
-    </div>
+    </Link>
   );
 };
--- features/dashboard/servers/components/discovery/DiscoverServersContent.tsx
+++ features/dashboard/servers/components/discovery/DiscoverServersContent.tsx
@@ -3,6 +3,9 @@ import { Center } from "@/components/custom/Center";
 import { H1, H3, H4 } from "@/components/typography";
 import React from "react";
 import { FormServerList } from "./components/FormServerList";
+import { Button } from "@/components/ui/button";
+import routes from "@/lib/routes";
+import Link from "next/link";
 
 export const DiscoverServersContent = () => {
   return (
@@ -12,7 +15,12 @@ export const DiscoverServersContent = () => {
           <EZLogoDark />
           <H1>EZComms</H1>
         </div>
-        <H3 className="font-bold">Discover new servers</H3>
+        <div className="flex flex-row gap-md">
+          <H3 className="font-bold">Discover new servers or</H3>
+          <Link href={routes.dashboard}>
+            <Button>Go to dashboard</Button>
+          </Link>
+        </div>
         <FormServerList />
       </div>
     </Center>
